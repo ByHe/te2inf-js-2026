af6ab87f71ca72261873b08fbbe33dd154bb87fb
Henrik Bygren <henrik.bygren@gmail.com>
2026-03-17 08:55:42 +0100

    init

---
+++ script/script.js
@@ -0,0 +1,27 @@
+//------- Click/innerText -----------
+const p1 = document.getElementById('p1');
+
+p1.onclick = changeTxT;
+
+function changeTxT(){
+   p1.innerText += " nytt efter klick";
+}
+
+const n1 = document.getElementById('n1');
+const n2 = document.getElementById('n2');
+
+// ------- Calc -------
+n1.onchange = sum;
+n2.onchange = sum;
+
+function sum(){
+   const output = document.getElementById('op1');
+
+   if(n1.value && n2.value){
+      output.innerText = Number(n1.value) + Number(n2.value);
+   }else{
+      output.innerText = '';
+   }
+}
+
+// ------- ul/li --------
