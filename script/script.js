75336cd7255a09296517078a2d8e6cc315d9e182
Henrik Bygren <henrik.bygren@gmail.com>
2026-03-17 13:36:41 +0100

    lek 1

--- script/script.js
+++ script/script.js
@@ -1,31 +1,46 @@
 //------- Click/innerText -----------
 const p1 = document.getElementById('p1');
 
-p1.onclick = changeTxT;
+p1.onclick = changeTxt;
 
-function changeTxT(){
+function changeTxt(){
    p1.innerText += " nytt efter klick";
 }
 
-// ------- calculator/value------- 1
-const n1 = document.getElementById('n1');
-const n2 = document.getElementById('n2');
+// ------- calculator/value------- 
+const num1 = document.getElementById('n1');
+const num2 = document.getElementById('n2');
 
-n1.onchange = sum;
-n2.onchange = sum;
+num1.onchange = sum;
+num2.onchange = sum;
 
 function sum(){
    const output = document.getElementById('op1');
 
-   if(n1.value && n2.value){
-      output.innerText = Number(n1.value) + Number(n2.value);
+   if(num1.value && num2.value){
+      output.innerText = Number(num1.value) + Number(num2.value);
    }else{
       output.innerText = '';
    }
 }
 
 //------- ul/li eventobjektet --------
-
 //------- target/currentTarget --------
+const ul = document.getElementById('ul1');
+ul.onclick = changeBg;
+
+function changeBg(event){
+   event.currentTarget.style.backgroundColor = "red";
+}
 
 //------- läs in flera element i en array --------
+
+const liArray = document.getElementsByTagName('li');
+
+for(let i = 0; i < liArray.length; i++){
+   liArray[i].onclick = changeColor;
+}
+
+function changeColor(e){
+   e.target.style.color = "green";
+}
